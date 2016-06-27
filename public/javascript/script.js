a036daee880f6a7866dbbdbf5bb368c2751aa66c
Kendra Lyndon <kendralyndon@gmail.com>
2016-06-27 11:07:18 -0600

    edit link css

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
   link.on('mouseenter',function(){
     // $(this).css('color','#0001F0');
     $(this).css('color','#E32643');
+    $(this).css('font-size',55)
   });
   link.on('mouseleave',function(){
     // $(this).css('color','#525AFF');
     $(this).css('color','#9E0D18');
+    $(this).css('font-size',45);
   })
 })
 
